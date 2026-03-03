852cdbe4abe5c478678d744f29e91d9c98c95049
Jack_up <36533859+GiacomoGuaresi@users.noreply.github.com>
2026-03-03 08:01:44 +0100

    Rewrite virtual keyboard component from scratch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'react-simple-keyboard/build/css/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
